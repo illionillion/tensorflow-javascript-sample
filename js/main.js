16bb4c9f67f26611b644c3036ff8d4fed6a12bad
yuto ichinose <yuto.0526.sano@gmail.com>
2023-12-15 17:45:02 +0900

    fix: formated

--- js/main.js
+++ js/main.js
@@ -1,51 +1,53 @@
-"use strict"
-import "tfjs"
-import "coco-ssd"
+"use strict";
+import "tfjs";
+import "coco-ssd";
 
-const handleFIleChange = async e => {
-    if(e.target.files.length === 0) return
-    const loading = document.getElementById("loading-container")
-    const resultPreview = document.getElementById("result-preview")
-    loading.classList.remove("none")
-    resultPreview.classList.add("none")
-    const file = e.target.files[0]
-    const imageObj = new Image()
-    imageObj.src = URL.createObjectURL(file)
+const handleFIleChange = async (e) => {
+  if (e.target.files.length === 0) return;
+  const loading = document.getElementById("loading-container");
+  const resultPreview = document.getElementById("result-preview");
+  loading.classList.remove("none");
+  resultPreview.classList.add("none");
+  const file = e.target.files[0];
+  const imageObj = new Image();
+  imageObj.src = URL.createObjectURL(file);
 
-    // モデルの読み込み
-    const model = await cocoSsd.load()
-    // 画像からオブジェクト認識
-    const predictions = await model.detect(imageObj)
-    console.log(predictions);
-    const canvas = document.getElementById("result-canvas")
-    canvas.width = imageObj.width
-    canvas.height = imageObj.height
-    const context = canvas.getContext("2d")
-    context.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height)
-    
-    // 描画
-    for (const p of predictions) {
-        if (p.class === "person") {
-            context.beginPath()
-            context.rect(...p.bbox)
-            context.lineWidth = 1
-            context.strokeStyle = "black"
-            context.fillStyle = "black"
-            context.stroke()
-            context.fill()
-        }
+  // モデルの読み込み
+  const model = await cocoSsd.load();
+  // 画像からオブジェクト認識
+  const predictions = await model.detect(imageObj);
+  console.log(predictions);
+  const canvas = document.getElementById("result-canvas");
+  canvas.width = imageObj.width;
+  canvas.height = imageObj.height;
+  const context = canvas.getContext("2d");
+  context.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height);
+
+  // 描画
+  for (const p of predictions) {
+    if (p.class === "person") {
+      context.beginPath();
+      context.rect(...p.bbox);
+      context.lineWidth = 1;
+      context.strokeStyle = "black";
+      context.fillStyle = "black";
+      context.stroke();
+      context.fill();
     }
+  }
 
-    const base64 = canvas.toDataURL("image/jpeg");
-    resultPreview.classList.remove("none")
-    document.getElementById("result-preview-image").src = base64;
-    document.getElementById("result-preview-download").href = base64;
-    document.getElementById("result-preview-download").download = file.name;
-    loading.classList.add("none")
-}
+  const base64 = canvas.toDataURL("image/jpeg");
+  resultPreview.classList.remove("none");
+  document.getElementById("result-preview-image").src = base64;
+  document.getElementById("result-preview-download").href = base64;
+  document.getElementById("result-preview-download").download = file.name;
+  loading.classList.add("none");
+};
 
 const main = async () => {
-    document.getElementById("file-input").addEventListener("change", handleFIleChange)
-}
+  document
+    .getElementById("file-input")
+    .addEventListener("change", handleFIleChange);
+};
 
-window.addEventListener("load", main)
+window.addEventListener("load", main);
