e4bd58d0927e4aca94aa5b7e4e4b28f91d2c2e27
yuto ichinose <yuto.0526.sano@gmail.com>
2023-12-15 12:28:38 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const handleFIleChange = async e => {
+    console.log(e.target.files);
+    if(e.target.files.length === 0) return
+    const file = e.target.files[0]
+    const imageObj = new Image()
+    imageObj.src = URL.createObjectURL(file)
+
+    // モデルの読み込み
+    const model = await cocoSsd.load()
+    // 画像からオブジェクト認識
+    const predictions = await model.detect(imageObj)
+    console.log(predictions);
+    const canvas = document.getElementById("result-canvas")
+    canvas.width = imageObj.width
+    canvas.height = imageObj.height
+    const context = canvas.getContext("2d")
+    context.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height)
+    
+    // 描画
+    for (const p of predictions) {
+        if (p.class === "person") {
+            context.beginPath()
+            context.rect(...p.bbox)
+            context.lineWidth = 1
+            context.strokeStyke = "red"
+            context.fillStyke = "red"
+            context.stroke()
+            context.fill()
+        }
+    }
+
+    const base64 = canvas.toDataURL("image/jpeg");
+    document.getElementById("result-preview").classList.remove("none")
+    document.getElementById("result-preview-image").src = base64;
+    document.getElementById("result-preview-download").href = base64;
+    document.getElementById("result-preview-download").download = file.name;
+}
+
+const main = async () => {
+    document.getElementById("file-input").addEventListener("change", handleFIleChange)
+}
+
+window.addEventListener("load", main)
