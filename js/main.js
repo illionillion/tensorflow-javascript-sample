adc13ffb901cd98b337e5c5ff3c39a522a100bf7
yuto ichinose <yuto.0526.sano@gmail.com>
2023-12-16 10:53:19 +0900

    feat: 複数画像対応

--- js/main.js
+++ js/main.js
@@ -2,52 +2,93 @@
 import "tfjs";
 import "coco-ssd";
 
-const handleFileChange = async (e) => {
-  if (e.target.files.length === 0) return;
-  const loading = document.getElementById("loading-container");
-  const resultPreview = document.getElementById("result-preview");
-  loading.classList.remove("none");
-  resultPreview.classList.add("none");
-  const file = e.target.files[0];
-  const imageObj = new Image();
-  imageObj.src = URL.createObjectURL(file);
+(() => {
+  let imageList = [];
+  let currentIndex = 0;
 
-  // モデルの読み込み
-  const model = await cocoSsd.load();
-  // 画像からオブジェクト認識
-  const predictions = await model.detect(imageObj);
-  console.log(predictions);
-  const canvas = document.getElementById("result-canvas");
-  canvas.width = imageObj.width;
-  canvas.height = imageObj.height;
-  const context = canvas.getContext("2d");
-  context.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height);
+  const handleFileChange = async (e) => {
+    if (e.target.files.length === 0) return;
+    const loading = document.getElementById("loading-container");
+    const resultPreview = document.getElementById("result-preview");
+    loading.classList.remove("none");
+    resultPreview.classList.add("none");
 
-  // 描画
-  for (const p of predictions) {
-    if (p.class === "person") {
-      context.beginPath();
-      context.rect(...p.bbox);
-      context.lineWidth = 1;
-      context.strokeStyle = "black";
-      context.fillStyle = "black";
-      context.stroke();
-      context.fill();
+    imageList = []
+    currentIndex = 0
+
+    for (let i = 0; i < e.target.files.length; i++) {
+      const file = e.target.files[i];
+      const imageObj = new Image();
+      imageObj.src = URL.createObjectURL(file);
+
+      // モデルの読み込み
+      const model = await cocoSsd.load();
+      // 画像からオブジェクト認識
+      const predictions = await model.detect(imageObj);
+      console.log(predictions);
+      const canvas = document.getElementById("result-canvas");
+      canvas.width = imageObj.width;
+      canvas.height = imageObj.height;
+      const context = canvas.getContext("2d");
+      context.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height);
+
+      // 描画
+      for (const p of predictions) {
+        if (p.class === "person") {
+          context.beginPath();
+          context.rect(...p.bbox);
+          context.lineWidth = 1;
+          context.strokeStyle = "black";
+          context.fillStyle = "black";
+          context.stroke();
+          context.fill();
+        }
+      }
+
+      const base64 = canvas.toDataURL("image/jpeg");
+      imageList.push({
+        name: file.name,
+        url: base64,
+      });
     }
-  }
 
-  const base64 = canvas.toDataURL("image/jpeg");
-  resultPreview.classList.remove("none");
-  document.getElementById("result-preview-image").src = base64;
-  document.getElementById("result-preview-download").href = base64;
-  document.getElementById("result-preview-download").download = file.name;
-  loading.classList.add("none");
-};
+    resultPreview.classList.remove("none");
+    document.getElementById("result-preview-image").src = imageList[currentIndex].url;
+    document.getElementById("result-preview-download").href = imageList[currentIndex].url;
+    document.getElementById("result-preview-download").download =
+      imageList[currentIndex].name;
+    loading.classList.add("none");
+  };
 
-const main = async () => {
-  document
-    .getElementById("file-input")
-    .addEventListener("change", handleFileChange);
-};
+  const prev = () => {
+    if (currentIndex === 0 || !imageList.length) return;
+    currentIndex--;
+    document.getElementById("result-preview-image").src =
+      imageList[currentIndex].url;
+    document.getElementById("result-preview-download").href =
+      imageList[currentIndex].url;
+    document.getElementById("result-preview-download").download =
+      imageList[currentIndex].name;
+  };
 
-window.addEventListener("load", main);
+  const next = () => {
+    if (currentIndex >= imageList.length - 1 || !imageList.length) return;
+    currentIndex++;
+    document.getElementById("result-preview-image").src =
+      imageList[currentIndex].url;
+    document.getElementById("result-preview-download").href =
+      imageList[currentIndex].url;
+    document.getElementById("result-preview-download").download =
+      imageList[currentIndex].name;
+  };
+
+  const main = async () => {
+    document
+      .getElementById("file-input")
+      .addEventListener("change", handleFileChange);
+    document.getElementById("prev-button").addEventListener("click", prev);
+    document.getElementById("next-button").addEventListener("click", next);
+  };
+
+  window.addEventListener("load", main);
+})();
